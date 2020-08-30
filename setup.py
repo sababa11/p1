d9883fbd37306ed3127bf28f209ad2185c32b301
adamt <adamtarasov@gmail.com>
2020-08-30 10:38:59 +0300

    changing structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='src1',
+    version='0.1',
+    description='Python Distribution Utilities',
+    author='Adam Ta',
+    author_email='notrelevant@gmail.net',
+    url='https://github.com/sababa11/p1.git',
+    install_requires=['requests==2.24.0'],
+    packages=['src1'],
+    )
+
+print("OK")
