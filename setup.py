517147957d4e8a60f173c26a53902ac64c191d2e
adamt <adamtarasov@gmail.com>
2020-08-30 10:50:36 +0300

    changing project structure 2

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(
-    name='src1',
+    name='p1',
     version='0.1',
     description='Python Distribution Utilities',
     author='Adam Ta',
     author_email='notrelevant@gmail.net',
     url='https://github.com/sababa11/p1.git',
     install_requires=['requests==2.24.0'],
-    packages=['src1'],
+    packages=['p2'],
     )
 
 print("OK")
