737d505a93b1ce9d0ef980d2ca229a026609641e
adamt <adamtarasov@gmail.com>
2020-08-30 11:03:27 +0300

    updating refereance

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 setup(
     name='p1',
     version='0.1',
-    description='Python Distribution Utilities',
+    description='Python Package 1',
     author='Adam Ta',
-    author_email='notrelevant@gmail.net',
+    author_email='notrelevant@mail.net',
     url='https://github.com/sababa11/p1.git',
     install_requires=['requests==2.24.0'],
-    packages=['p2'],
+    packages=['p1'],
     )
 
 print("OK")
